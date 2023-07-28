ba06d2c73bdce842cad968a1d0a46647ba0d3f03
SDR <50109097+dmytrosenyk@users.noreply.github.com>
2023-07-28 14:16:45 +0300

    update main

--- src/main/java/com/senyk/puzzle/Main.java
+++ src/main/java/com/senyk/puzzle/Main.java
@@ -15,33 +15,32 @@ public class Main {
     String imgPath = "src/main/resources/f1.jpg";
     int rows =4;
     int columns =4;
+
     LinkedList<LinkedList<Image>> puzzles =new LinkedList<>();
     BufferedImage image = ReadImg.readFile(imgPath);
     LinkedList<BufferedImage> bufferedImages = DivisionImage.divisionImageIntoParts(image,rows,columns);
-    System.out.println(bufferedImages.size());
-    Collections.shuffle(bufferedImages);
     LinkedList<Image> images = bufferedImages.stream()
         .map(Image::new)
         .collect(Collectors.toCollection(LinkedList::new));
+    for (int i = 0; i < images.size(); i++) {
+      images.get(i).setIndex(i);
+    }
+    Collections.shuffle(images);
+
     for (int i = 0; i < rows; i++) {
       puzzles.add(PuzzleBuilder.buildRow(images,columns));
+      puzzles.forEach(System.out::println);
+      System.out.println();
     }
     List<Image> result = new java.util.ArrayList<>(PuzzleBuilder.buildPuzzle(puzzles, rows)
         .stream()
         .flatMap(LinkedList::stream)
-        .toList());;
-    Collections.reverse(result);
+        .toList());
+
     for (int i = 0; i < result.size(); i++) {
       String name = "src/main/resources/sub-img-"+i+".jpg";
       File outputfile = new File(name);
       ImageIO.write(result.get(i).getImage(), "jpg", outputfile);
     }
-//    for (int i = 0; i < rows; i++) {
-//      for (int j = 0; j < columns; j++) {
-//        String name = "src/main/resources/sub-img-"+i+"-"+j+".jpg";
-//        File outputfile = new File(name);
-//        ImageIO.write(result.get(i*(columns-1)+j).getImage(), "jpg", outputfile);
-//      }
-//    }
   }
 }
